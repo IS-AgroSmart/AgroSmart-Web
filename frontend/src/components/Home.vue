9f66a2a5ffa7ad7f14bd9ed5133fb57d07899f47
Manuel Lecaro <mnlecaro@gmail.com>
2019-12-19 11:26:59 -0500

    componente base

---
+++ frontend/src/components/Home.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Home",
+
+    props: {
+
+    },
+
+    data() {
+        return {
+            
+        }
+    },
+
+    methods: {
+        readNews(newsNum){
+            this.$emit('read', newsNum);
+        }
+    }
+}
+</script>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,5 +1,4 @@
 import Vue from 'vue'
-import App from './App.vue'
 import VueRouter from 'vue-router'
 import Home from './components/Home.vue'
 
