d3c5d0a5125a9d4e03b36aef88bcac5f3b9fba82
jreyesr <jreyesr@users.noreply.github.com>
2020-08-04 15:31:03 -0500

    BUGFIX: Add cast to str on credentials

--- IngSoft1/settings.py
+++ IngSoft1/settings.py
@@ -167,7 +167,7 @@ STATIC_ROOT = 'staticfiles'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = config('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+EMAIL_HOST_USER = config('EMAIL_HOST_USER', cast=str)
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', cast=str)
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 #EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
