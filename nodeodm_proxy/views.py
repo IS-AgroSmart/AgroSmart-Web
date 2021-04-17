d0f013be848d1d292bd0809778db90da0492bfd1
jreyesr <jreyesr@users.noreply.github.com>
2021-04-16 21:58:01 -0500

    Authenticate NodeODM calls on backend

--- nodeodm_proxy/views.py
+++ nodeodm_proxy/views.py
@@ -5,15 +5,21 @@ from django.http import HttpResponse, JsonResponse
 import requests
 
 from django.views.decorators.csrf import csrf_exempt
+from rest_framework.authtoken.models import Token
 
-from core.models import Flight, FlightState
+from core.models import Flight, FlightState, UserType
 from nodeodm_proxy import api
 
 _NODEODM_STATUS_CODES = {FlightState.COMPLETE.name: 40, FlightState.ERROR.name: 30, FlightState.CANCELED.name: 50}
 
 
 def task_info(request, uuid):
+    user = Token.objects.get(key=request.headers["Authorization"][6:]).user
     flight = Flight.objects.get(uuid=uuid)
+
+    if not user.type == UserType.ADMIN.name and not flight.user == user:
+        return HttpResponse(status=403)
+
     if flight.state in _NODEODM_STATUS_CODES:  # Flight has ended, return cached values
         data = {
             "status": {"code": _NODEODM_STATUS_CODES[flight.state]},
@@ -27,7 +33,11 @@ def task_info(request, uuid):
 
 
 def task_output(request, uuid):
+    user = Token.objects.get(key=request.headers["Authorization"][6:]).user
     flight = Flight.objects.get(uuid=uuid)
+
+    if not user.type == UserType.ADMIN.name and not flight.user == user:
+        return HttpResponse(status=403)
 
     if flight.state in _NODEODM_STATUS_CODES:  # Flight has ended, return hardcoded value
         return HttpResponse(b"Vuelo completo")
@@ -39,8 +49,14 @@ def task_output(request, uuid):
 
 @csrf_exempt
 def cancel_task(request):
+    user = Token.objects.get(key=request.headers["Authorization"][6:]).user
     data = json.loads(request.body.decode("utf-8"))
     uuid = data["uuid"]
+    flight = Flight.objects.get(uuid=uuid)
+
+    if not user.type == UserType.ADMIN.name and not flight.user == user:
+        return HttpResponse(status=403)
+
     response = requests.post(f"{settings.NODEODM_SERVER_URL}/task/cancel?token={settings.NODEODM_SERVER_TOKEN}",
                              data={"uuid": uuid})
     return HttpResponse(status=response.status_code)
